157c3b0094adaa356f36b30b2d8e8de5b6cf5c3f
vritant <vritoku@gmail.com>
2026-01-30 19:04:58 -0800

    init vite + ts + jquery project

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import $ from "jquery";
+import "./style.css";
+
+$("#app").html(`
+  <div>
+    <h1>Vite + TypeScript + jQuery</h1>
+    <p>Edit <code>src/main.ts</code> and save.</p>
+  </div>
+`);
