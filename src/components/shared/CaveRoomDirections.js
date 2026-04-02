7d811940a313ce878989149f01a1ee50eb5a3fc0
jnaftolin <jordan.naftolin@gmail.com>
2026-04-02 23:10:15 +0000

    Add a shared folder containing a file with CaveRoom directions that can be used across the objects

---
+++ src/components/shared/CaveRoomDirections.js
@@ -0,0 +1,10 @@
+
+// define cave room directions that a user can navigate or shoot
+export const CaveRoomDirections = {
+    NORTH: 'north',
+    NORTH_EAST: 'north_east',
+    SOUTH_EAST: 'south_east',
+    SOUTH: 'south',
+    SOUTH_WEST: 'south_west',
+    NORTH_WEST: 'north_west'
+};
